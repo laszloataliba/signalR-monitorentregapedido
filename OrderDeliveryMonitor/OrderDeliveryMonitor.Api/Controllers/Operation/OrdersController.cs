426923fd2c17bbd71e253808bdc26b4aa1ad8716
INTERNATIONAL\lataliba <lataliba@cinemark.com.br>
2020-05-22 19:38:25 -0300

    Ajuste na arquitetura das APIs

--- OrderDeliveryMonitor/OrderDeliveryMonitor.Api/Controllers/Operation/OrdersController.cs
+++ OrderDeliveryMonitor/OrderDeliveryMonitor.Api/Controllers/Operation/OrdersController.cs
@@ -10,36 +10,74 @@ using System.Threading.Tasks;
 
 namespace OrderDeliveryMonitor.Api.Controllers.Operation
 {
-    [Route("api/Operation/Orders")]
-    [ApiController]
+    /// <summary>
+    /// Order controller.
+    /// </summary>
+    [Route("api/Operation/Orders"),
+     ApiController]
     public class OrdersController : ControllerBase
     {
+        #region :: Fields ::
+
+        /// <summary>
+        /// Hub access object.
+        /// </summary>
         private readonly IHubContext<OrderDeliveryMonitorHub> _hubContext;
+
+        /// <summary>
+        /// Order model facade object.
+        /// </summary>
         private readonly IFOrder fOrder;
 
+        #endregion :: Fields ::
+
+        #region :: Methods ::
+
+        /// <summary>
+        /// Constructor method.
+        /// </summary>
+        /// <param name="hubContext">Hub access obejct parameter.</param>
+        /// <param name="pFOrder">Order model facade parameter.</param>
         public OrdersController(IHubContext<OrderDeliveryMonitorHub> hubContext, IFOrder pFOrder)
         {
             _hubContext = hubContext;
             fOrder = pFOrder;
         }
 
-        // GET: api/Order
+        #endregion :: Methods ::
+
+        #region :: Actions ::
+
+        /// <summary>
+        /// Recovers the orders that is on waiting status.
+        /// </summary>
+        /// <returns>Order list.</returns>
         [HttpGet]
-        public IEnumerable<OrderDTO> Get() //Testar IActionResult como retorno
+        public IEnumerable<OrderDTO> Get()
         {
             var vOrders = fOrder.GetListOrderDTO(pInclude: itm => itm.Items);
 
             return vOrders;
         }
 
-        [HttpGet("{id}")]
-        public OrderDTO Get(int id)
+        /// <summary>
+        /// Recovers the order data by given identifier.
+        /// </summary>
+        /// <param name="pOrderId">Order identifier.</param>
+        /// <returns>Order data.</returns>
+        [HttpGet("{pOrderId}")]
+        public OrderDTO Get(string pOrderId)
         {
-            var vOrder = fOrder.GetOrderDTO(order => order.OrderId == id, items => items.Items);
+            var vOrder = fOrder.GetOrderDTO(order => order.OrderId == int.Parse(pOrderId), items => items.Items);
 
             return vOrder;
         }
 
+        /// <summary>
+        /// Changes order status.
+        /// </summary>
+        /// <param name="pOrderId">Order identifier.</param>
+        /// <returns></returns>
         [HttpPut("{pOrderId}")]
         public async Task Put(string pOrderId)
         {
@@ -47,5 +85,7 @@ namespace OrderDeliveryMonitor.Api.Controllers.Operation
 
             await _hubContext.Clients.All.SendAsync($"{AppUtilities.RELOAD_AWAITING_CONTAINER}");
         }
+
+        #endregion :: Actions ::
     }
 }
--- OrderDeliveryMonitor/OrderDeliveryMonitor.Api/Controllers/Security/UsersController.cs
+++ OrderDeliveryMonitor/OrderDeliveryMonitor.Api/Controllers/Security/UsersController.cs
@@ -1,55 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using OrderDeliveryMonitor.Api.Hubs;
+using System.Collections.Generic;
 
 namespace OrderDeliveryMonitor.Api.Controllers.Security
 {
-    [Route("api/Security/Users")]
-    [ApiController]
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/Security/Users"),
+     ApiController]
     public class UsersController : ControllerBase
     {
+        #region :: Fields ::
+
+        /// <summary>
+        /// 
+        /// </summary>
         private readonly IHubContext<OrderDeliveryMonitorHub> _hubContext;
 
+        #endregion :: Fields ::
+
+        #region :: Methods ::
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hubContext"></param>
         public UsersController(IHubContext<OrderDeliveryMonitorHub> hubContext)
         {
             _hubContext = hubContext;
         }
 
-        // GET: api/User
+        #endregion :: Methods ::
+
+        #region :: Actions ::
+
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/User/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/User
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/User/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
+
+        #endregion :: Actions ::
     }
 }
